5f1e4175e4feaf00d4a5cd85fc12184d476b0f70
jbrennan414 <brennanj414@gmail.com>
2024-12-17 20:18:21 -0700

    changes

--- mymodule/main.go
+++ mymodule/main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+func fetchCatFacts() string {
+
+	getURL := "https://cat-fact.herokuapp.com/facts"
+	response, err := http.Get(getURL)
+	if err != nil {
+		fmt.Println("Error making get request: ", err)
+		return "Error making request"
+	}
+
+	defer response.Body.Close()
+
+	// read and print the response
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response", err)
+		return "Error reading response"
+	}
+	return string(body)
+
+}
+
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello Cloudflare!")
+		catFacts := fetchCatFacts()
+		fmt.Fprintf(w, catFacts)
 	})
 
 	fmt.Println("Serving on port 5555")
